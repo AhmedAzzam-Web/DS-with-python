710c6e953ce0f7f0d3b010b7dec5e0a8f9d9c806
Ahmed Azzam <ahmedazzam.business@gmail.com>
2024-02-23 03:12:41 +0200

    Check if stack empty or not method

--- stack.py
+++ stack.py
@@ -12,6 +12,14 @@ class Stack:
     def getTop(self):
         print(Stack.stack[-1])
         return Stack.stack[-1]
+    
+    def isEmpty(self):
+        if len(Stack.stack):
+            print('There are elements in stack')
+            return True
+        else:
+            print('Stack is empty')
+            return False
 
 
 stack = Stack()
@@ -23,3 +31,7 @@ print(stack.stack)
 stack.pop()
 
 print(stack.stack)
+
+stack.getTop()
+
+stack.isEmpty()
