bbcdd442b404a4c28e99c7e86e46eccbc834f5d5
Ahmed Azzam <ahmedazzam.business@gmail.com>
2024-02-23 03:08:42 +0200

    Finished the pop method

--- stack.py
+++ stack.py
@@ -6,4 +6,16 @@ class Stack:
         for element in elements:
             Stack.stack.append(element)
 
-    
+    def pop(self):
+        Stack.stack.pop()
+
+
+stack = Stack()
+
+stack.push(1,2,3)
+
+print(stack.stack)
+
+stack.pop()
+
+print(stack.stack)
